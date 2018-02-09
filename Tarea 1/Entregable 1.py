48b31f19b2efa746ed482c245c45206bd0538c5d
ChuiiZeet <chuy.guerra19@outlook.es>
2018-02-09 07:04:00 -0600

    Func explotar

--- Tarea 1/Entregable 1.py
+++ Tarea 1/Entregable 1.py
@@ -1,4 +1,4 @@
-#Modificar el codigo para el entregable 
+#Problema!
 import math
 import random
 import pygame
@@ -7,12 +7,15 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 800))
 particles = [(random.gauss(0,.5), random.uniform(0,6.28318)) for i in range(2000)]
+def explotar():
+    for i in range(399):
+        screen.fill((255,255,255))
+        for speed, angle in particles:
+            distance = i * speed
+            x = 400 + distance * math.cos(angle)
+            y = 400 + distance * math.sin(angle)
+            screen.set_at((int(x), int(y)), (0,0,0))
+        pygame.display.flip()
 
-for i in range(399):
-    screen.fill((255,255,255))
-    for speed, angle in particles:
-        distance = i * speed
-        x = 400 + distance * math.cos(angle)
-        y = 400 + distance * math.sin(angle)
-        screen.set_at((int(x), int(y)), (0,0,0))
-    pygame.display.flip()
+
+explotar()
