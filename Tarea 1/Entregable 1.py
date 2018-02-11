e94174fe3e5cd4b92186c938ace5f7c68d6e2e90
ChuiiZeet <chuy.guerra19@outlook.es>
2018-02-11 15:58:13 -0600

    Secuencia

--- Tarea 1/Entregable 1.py
+++ Tarea 1/Entregable 1.py
@@ -2,15 +2,18 @@
 import math
 import random
 import pygame
+import sys
 
 pygame.init()
 
 screen = pygame.display.set_mode((800, 800))
 particles = [(random.gauss(0,.5), random.uniform(0,6.28318)) for i in range(1500)]
+#(random.gauss(0,.5), random.uniform(0,6.28318))
+
 
 def explotar():
     for i in range(399):
-        screen.fill((255,255,255))
+        screen.fill((0,0,0))
         for speed, angle in particles:
             distance = i * speed
             x = 400 + distance * math.cos(angle)
@@ -18,5 +21,8 @@ def explotar():
             screen.set_at((int(x), int(y)), (200,0,0)) #Color
         pygame.display.flip()
 
+def secuencia_explociones(n):
+    for x in range(0,n):
+        explotar()
 
-explotar()
+secuencia_explociones(2)
