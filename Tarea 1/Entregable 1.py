ed0c810e0f6f46a170e4149267cbdbdabf2b01e3
jesus guerra <chuy.guerra19@outlook.es>
2018-02-11 21:16:49 -0600

    close

--- Tarea 1/Entregable 1.py
+++ Tarea 1/Entregable 1.py
@@ -8,21 +8,54 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 800))
 particles = [(random.gauss(0,.5), random.uniform(0,6.28318)) for i in range(1500)]
-#(random.gauss(0,.5), random.uniform(0,6.28318))
 
+#Colores RGB
+Colores = [[255,87,51],[88,255,51],[243,0,255],[255, 102, 255],[204, 0, 102],[255, 255, 0],[0, 204, 0],[153, 0, 255],[0, 51, 0],[51, 51, 204],
+[255,87,51],[88,255,51],[243,0,255],[255, 102, 255],[204, 0, 102],[255, 255, 0],[0, 204, 0],[153, 0, 255],[0, 51, 0],[51, 51, 204],
+[255,87,51],[88,255,51],[243,0,255],[255, 102, 255],[204, 0, 102],[255, 255, 0],[0, 204, 0],[153, 0, 255],[0, 51, 0],[51, 51, 204]]
 
-def explotar():
-    for i in range(399):
+#Funcion explotar
+def explotar(n,posx,posy,rad):
+    for i in range(rad):
         screen.fill((0,0,0))
         for speed, angle in particles:
             distance = i * speed
-            x = 400 + distance * math.cos(angle)
-            y = 400 + distance * math.sin(angle)
-            screen.set_at((int(x), int(y)), (200,0,0)) #Color
+            x = posx + distance * math.cos(angle)
+            y = posy + distance * math.sin(angle)
+            screen.set_at((int(x), int(y)), (Colores[n])) #Color
         pygame.display.flip()
 
+#Funcion Secuencia_explociones
 def secuencia_explociones(n):
     for x in range(0,n):
-        explotar()
+        posx = random.randint(1,400)
+        posy = random.randint(1,400)
+        rad = random.randint(1,399)
+        #Coordenadas
+        print("\n")
+        print("La coordenada en x de la explosion ", x, "es:", posx)
+        print("La coordenada en y de la explosion ", x, "es:", posy)
+        print("El radio de la explosion ", x, "es:", rad)
+        print("=======================================================\n")
+        explotar(x,posx,posy,rad)
 
-secuencia_explociones(15)
+#funcion secuencia explosiones linea recta
+def secuencia_explociones_linea():
+    y = 100
+    c = 0
+    x = 0
+    rad = 399
+    for y in range(0,400):
+        y += 100
+        c += 1
+        explotar(c,x,y,rad)
+
+
+#Main
+def main():
+    if len(sys.argv) == 1:
+        print("Explosiones random")
+        secuencia_explociones(12)
+    else:
+        secuencia_explociones(int(sys.argv[1]))
+main()
